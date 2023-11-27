04c11352604ef2ec4e0c966769c83f5597b6ae07
juane_lezca <juanelezca@gmail.com>
2023-11-27 17:48:33 -0300

    lo rompi

--- guia 5 parte B/ejercicio1/simulation/qsim/work/ejercicio1_diagram_block_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ejercicio1_diagram_block_vlg_vec_tst is
-end ejercicio1_diagram_block_vlg_vec_tst;
--- guia 5 parte B/ejercicio2/simulation/qsim/work/sumador_completo_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity sumador_completo_vlg_vec_tst is
-end sumador_completo_vlg_vec_tst;
